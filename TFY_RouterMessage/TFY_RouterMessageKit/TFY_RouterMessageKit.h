8fcf47989a08c63367c51da813f562e23cb2ce25
420144542@qq.com <420144542@qq.com>
2020-08-27 17:12:56 +0800

    组件化路由，消息监听，封装好的工具
    组件化路由，消息监听，封装好的工具

---
+++ TFY_RouterMessage/TFY_RouterMessageKit/TFY_RouterMessageKit.h
@@ -0,0 +1,17 @@
+//
+//  TFY_RouterMessageKit.h
+//  TFY_MessageMKit
+//
+//  Created by 田风有 on 2020/8/23.
+//  最新版本：2.0.0
+
+#ifndef TFY_RouterMessageKit_h
+#define TFY_RouterMessageKit_h
+
+//消息发送机制
+#import "TFY_MessageMKit.h"
+//路由
+#import "TFY_RouterMKit.h"
+
+
+#endif /* TFY_RouterMessageKit_h */
