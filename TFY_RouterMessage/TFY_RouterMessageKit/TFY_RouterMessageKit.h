d1ca098562875ebba68489189d4e6ab4e2ca77fc
420144542@qq.com <420144542@qq.com>
2020-09-10 16:35:16 +0800

    优化

--- TFY_RouterMessage/TFY_RouterMessageKit/TFY_RouterMessageKit.h
+++ TFY_RouterMessage/TFY_RouterMessageKit/TFY_RouterMessageKit.h
@@ -3,15 +3,26 @@
 //  TFY_MessageMKit
 //
 //  Created by 田风有 on 2020/8/23.
-//  最新版本：2.0.0
+//  最新版本：2.0.2
 
-#ifndef TFY_RouterMessageKit_h
-#define TFY_RouterMessageKit_h
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double TFY_RouterMessageKitVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char TFY_RouterMessageKitVersionString[];
+
+#define TFY_RouterMessageKitRelease 0
+
+#if TFY_RouterMessageKitRelease
+
+#import <TFY_MessageMKit/TFY_MessageMKit.h>
+#import <TFY_RouterMKit/TFY_RouterMKit.h>
+
+#else
 
 //消息发送机制
 #import "TFY_MessageMKit.h"
 //路由
 #import "TFY_RouterMKit.h"
 
-
-#endif /* TFY_RouterMessageKit_h */
+#endif
